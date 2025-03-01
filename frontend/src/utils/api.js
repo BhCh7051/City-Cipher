b4d33cc7594353398d89c1e279b6e79f71bfdddc
Bharat Chandwani <bharatchandwani1@gmail.com>
2025-03-02 01:12:53 +0530

    init

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for API - change based on your deployment
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://city-cipher-api.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
